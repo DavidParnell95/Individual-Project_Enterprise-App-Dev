d9478f584923f7a1f31e84fc92783e05b3e2eafa
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2020-04-15 23:08:30 +0100

    node + express set up

--- game-guides/app.js
+++ game-guides/app.js
@@ -1,41 +1,41 @@
-const http = require('http');
-const fs = require('fs');
-const port = 8080;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var admin = require('firebase-admin');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type' : 'text/html'})
-    fs.readFile('Home.html', function(error,data){
-        if(error)
-        {
-            res.writeHead(404);
-            res.write("ERROR: FILE NOT FOUND");
-        }
+var app = express();
 
-        else
-        {
-            res.write(data);
-        }
-        
-        res.end();
-    })
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-//Set server to listen
-server.listen(port, function(error)
-{
-    if(error)
-    {
-        console.log("ERROR: ", error);
-    }
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    else{
-        console.log('Server is listening on port: '+ port);
-    }
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://quarentine-games.firebaseio.com'
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
